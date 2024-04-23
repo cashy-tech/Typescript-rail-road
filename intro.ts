067b90977bc05616a62c04b59fc9250b56cf748d
Carolyne Ndunge <carohndunge@gmail.com>
2024-04-23 22:02:41 +0300

    Handling the primitives and "any"

--- intro.ts
+++ intro.ts
@@ -9,4 +9,13 @@ let userId: number = 7839;
 //boolean
 let isLoggeIn: boolean = false;
 
+//any: not a good practice! 
+let hero;
+
+function getHero() {
+  return "Horray";
+}
+hero = getHero();
+console.log(hero);
+
 export {};
