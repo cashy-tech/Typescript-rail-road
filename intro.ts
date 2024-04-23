da1667c68a460ec753a2b9f256490b4205abe012
Carolyne Ndunge <carohndunge@gmail.com>
2024-04-23 21:51:23 +0300

    Type annotations on variables

---
+++ intro.ts
@@ -0,0 +1,12 @@
+//string
+let greetings: string = "Hello Cashy";
+greetings.toLowerCase();
+console.log(greetings);
+
+//number
+let userId: number = 7839;
+
+//boolean
+let isLoggeIn: boolean = false;
+
+export {};
