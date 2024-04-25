be94f269710600a3256c8a52c798b5db58a3a2fd
Carolyne Ndunge <carohndunge@gmail.com>
2024-04-25 15:04:50 +0300

    Tuples in typescript

---
+++ myTuples.ts
@@ -0,0 +1,10 @@
+let user: [number , string , boolean] 
+user= [23, "cashy", true];
+
+let rgb:[number, number, number] = [255, 123, 112]
+
+type User = [number, string]
+
+const newUser:User = [122,"gj"]
+
+export {};
